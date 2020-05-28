c356a387ae7f7e711561337dc3ea98567d3a92a0
Ivan Janković <ivan.jankovic@esrlabs.com>
2020-05-28 13:51:48 +0200

    Decoder

--- day_4/exercise_4_1/src/decode.rs
+++ day_4/exercise_4_1/src/decode.rs
@@ -4,6 +4,89 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
+use std::vec;
+
+
+struct Message {
+    version: u8,
+    tp: u8,
+    reserved: u8,
+    id1: u8,
+    id2: u32,
+    timestamp: u32,
+    data: Vec<u8>,
+}
+
+impl Message {
+    pub fn new() -> Self {
+        Message {
+            version: 0,
+            tp: 0,
+            reserved: 0,
+            id1: 0,
+            id2: 0,
+            timestamp: 0,
+            data : Vec::new(),
+        }
+    }
+}
+
+struct Decoder {
+    state: u8,
+    msg : Message,
+}
+
+impl Decoder {
+    
+    pub fn new() -> Self {
+        Decoder {
+            state : 0,
+            msg : Message::new(),
+        }
+    }
+    
+    pub fn decode(&mut self, src: &[u8]) {
+        
+        for c in src.iter() {
+            
+            dbg!(self.state, c);
+            
+            match self.state {
+                
+                0 => {
+                    self.msg.version = *c;
+                    self.state += 1;
+                }
+                
+                1 => {
+                    self.msg.tp = *c;
+                    self.state += 1;
+                }
+                
+                2 => {
+                    self.msg.reserved = *c;
+                    self.state += 1;
+                }
+                
+                3 => {
+                    self.msg.id1 = *c;
+                    self.state += 1;
+                }
+                
+//                 0 => {
+//                     self.msg.version = *c;
+//                     self.state += 1;
+//                 }
+                
+                
+                _ => {println!("Bad state");}
+            }
+        }
+        
+    }
+}
+
+
 fn main() {
     
     println!("----------");
@@ -26,7 +109,7 @@ fn main() {
     
     let mut file = match File::open(file_name) {
         Ok(f) => f,
-        Err(e) => {
+        Err(_) => {
             println!("Error oppen file {}", file_name);
             std::process::exit(1);
         }
@@ -36,8 +119,19 @@ fn main() {
     
     println!("File opened");
     
+    let mut dc = Decoder::new();
     
     let mut buf=[0u8;12];
     
-    file.read(&mut buf).unwrap();
+    let rd = file.read(&mut buf);
+    match rd {
+        Err(_) => {
+            println!("Error READING");
+        }
+        Ok(n) => {
+            dc.decode(&buf[0..n]);
+        }
+    }
+    
+    
 }
