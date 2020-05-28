4bcaf19f2cb04b0be17d22fb0117f324005d75f1
Ivan Janković <ivan.jankovic@esrlabs.com>
2020-05-28 12:48:42 +0200

    Argument

--- day_4/exercise_4_1/src/decode.rs
+++ day_4/exercise_4_1/src/decode.rs
@@ -1,3 +1,19 @@
+
+use std::env;
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    
+    println!("----------");
+    println!("Decode");
+    println!("");
+    
+    
+    let args_vec: Vec<String> = env::args().collect();
+    println!("Args {:?}", args_vec);
+    
+    if args_vec.len() != 2 {
+        println!("Wrong argument");
+        std::process::exit(1);
+    }
 }
